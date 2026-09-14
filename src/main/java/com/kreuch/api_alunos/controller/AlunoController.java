f9043c0ff1e89675c333599d75c54a1491c6d0c6
Gustavo Kreuch <gustavokreuch40@gmail.com>
2026-09-14 20:27:32 -0300

    Initial commit

---
+++ src/main/java/com/kreuch/api_alunos/controller/AlunoController.java
@@ -0,0 +1,35 @@
+package com.kreuch.api_alunos.controller;
+
+import java.util.List;
+
+import com.kreuch.api_alunos.dto.AlunoRequest;
+import org.springframework.web.bind.annotation.*;
+
+import com.kreuch.api_alunos.dto.AlunoResponse;
+import com.kreuch.api_alunos.service.AlunoService;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+	private final AlunoService service;
+
+	public AlunoController(AlunoService service) {
+		this.service = service;
+	}
+
+	@GetMapping
+	public List<AlunoResponse> listarAlunos() {
+		return service.listarAlunos();
+	}
+
+	@GetMapping("/{id}")
+	public AlunoResponse obterAlunoPorId(@PathVariable int id) {
+		return service.obterAlunoPorId(id);
+	}
+
+	@PostMapping("/cadastrar")
+	public AlunoResponse cadastarAluno(@RequestBody AlunoRequest request){
+		return service.cadastrarAluno(request);
+	}
+}
---
+++ src/main/java/com/kreuch/api_alunos/service/AlunoService.java
@@ -0,0 +1,55 @@
+package com.kreuch.api_alunos.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kreuch.api_alunos.dto.AlunoRequest;
+import org.springframework.stereotype.Service;
+
+import com.kreuch.api_alunos.dto.AlunoResponse;
+import com.kreuch.api_alunos.model.Aluno;
+
+@Service
+public class AlunoService {
+
+	private final List<Aluno> alunos;
+
+	private int id = 1;
+
+	public AlunoService() {
+		alunos = new ArrayList<Aluno>();
+	}
+
+	public List<AlunoResponse> listarAlunos() {
+		List<Aluno> alunosModel = alunos;
+
+		List<AlunoResponse> alunosResponse = new ArrayList<>();
+
+		for (Aluno a : alunosModel) {
+			alunosResponse
+					.add(new AlunoResponse(a.getId(), a.getNome(), a.getEmail(), a.getDataNascimento(), a.getMedia()));
+		}
+		return alunosResponse;
+
+	}
+
+	public AlunoResponse obterAlunoPorId(int id) {
+		for (Aluno a : alunos) {
+			if (a.getId() == id) {
+				return new AlunoResponse(id, a.getNome(), a.getEmail(), a.getDataNascimento(), a.getMedia());
+			}
+		}
+		return null;
+	}
+
+	public AlunoResponse cadastrarAluno(AlunoRequest request){
+		alunos.add(new Aluno(id, request.getNome(), request.getEmail(), request.getSenha(), request.getDataNascimento(), request.getMedia()));
+
+		id++;
+		Aluno alunoCadastrado = alunos.getLast();
+
+		return new AlunoResponse(alunoCadastrado.getId(), alunoCadastrado.getNome(), alunoCadastrado.getEmail(), alunoCadastrado.getDataNascimento(), alunoCadastrado.getMedia());
+	}
+
+}
