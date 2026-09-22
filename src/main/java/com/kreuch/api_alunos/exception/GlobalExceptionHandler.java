4a7339e7716719ecd86a10d33cc3c6615a320768
GUSTAVO HENRIQUE KREUCH <gustavo_h_kreuch@estudante.sesisenai.org.br>
2026-09-21 21:38:50 -0300

    add ErrorResponse record & use it in exception handler

--- src/main/java/com/kreuch/api_alunos/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kreuch/api_alunos/exception/GlobalExceptionHandler.java
@@ -5,17 +5,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AlunoNaoEncontradoException.class)
-    public ResponseEntity<String> handleAlunNaoEncontradoException(AlunoNaoEncontradoException ex){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleAlunNaoEncontradoException(AlunoNaoEncontradoException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(404, ex.getMessage(), Instant.now()));
     }
 
     @ExceptionHandler(EmailJaCadastradoException.class)
-    public ResponseEntity<String> handleEmailJaCadastradoException(EmailJaCadastradoException ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleEmailJaCadastradoException(EmailJaCadastradoException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(409,ex.getMessage(),Instant.now()));
     }
 
 
